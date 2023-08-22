90ad764e81034f2604d7bfb1ca1452caaed41acb
william hill <williamhill0831@Hotmail.com>
2023-08-21 23:31:08 -0400

    creating knight moves

--- script.js
+++ script.js
@@ -15,4 +15,35 @@ const ChessSquare = (x, y) => {
     [-2, 1],
     [-2, -1],
   ];
+
+  const getPredecessor = () => predecessor;
+  const setPredecessor = (newPred) => {
+    predecessor ||= newPred;
+  };
+
+  const name = () => `${x}, ${y}`;
+
+  const createKnightMoves = () => {
+    return KNIGHT_OFFSETS.map(newSquareFrom).filter(Boolean);
+  };
+
+  const newSquareFrom = ([xOffset, yOffset]) => {
+    const [newX, newY] = [xPos + xOffset, yPos + yOffset];
+    if (0 <= newX && newX < 8 && 0 <= newY && y < 8) {
+      return ChessSquare(newX, newY);
+    }
+  };
+
+  if (squareRegistry.has(name())) {
+    return squareRegistry.get(name());
+  } else {
+    const newSquare = {
+      name,
+      getPredecessor,
+      setPredecessor,
+      createKnightMoves,
+    };
+    squareRegistry.set(name(), newSquare);
+    return newSquare;
+  }
 };
