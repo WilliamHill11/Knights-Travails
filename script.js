76c2f0898fac8a804e8db149238e9443fc3a039a
william hill <williamhill0831@Hotmail.com>
2023-08-24 23:03:37 -0400

    orgin

--- script.js
+++ script.js
@@ -46,4 +46,30 @@ const ChessSquare = (x, y) => {
     squareRegistry.set(name(), newSquare);
     return newSquare;
   }
+
+  const knightsTravails = (start, finish) => {
+    squareRegistry.clear();
+
+    const origin = ChessSquare(...start);
+    const target = ChessSquare(...finish);
+
+    const queue = [target];
+    while (!queue.includes(origin)) {
+      const currentSquare = queue.shift();
+
+      const enqueueList = currentSquare.createKnightMoves();
+      enqueueList.forEach((square) => square.setPredecessor(currentSquare));
+      queue.push(...enqueueList);
+    }
+    const path = [origin];
+    while (!path.includes(target)) {
+      const nextSquare = path.at(-1).getPredecessor();
+      path.push(nextSquare);
+    }
+    console.log(`The shortest path was ${path.length - 1} moves!`);
+    console.log('The moves were:');
+    path.forEach((square) => console.log(square.name()));
+  };
 };
+
+module.exports = knightsTravails;
